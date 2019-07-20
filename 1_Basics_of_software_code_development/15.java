53eabb07bec9e2aadc55662f6d86ea7b8aba87bb
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-07-20 19:59:04 +0300

    task 15 update

--- 1_Basics_of_software_code_development/15.java
+++ 1_Basics_of_software_code_development/15.java
@@ -1,16 +1,13 @@
 import java.math.BigDecimal;
 
 public class Main {
-    public static BigDecimal f() {
-        BigDecimal m = BigDecimal.ONE;
-        for (int i=1; i<=200; ++i) {
-            BigDecimal a = new BigDecimal(Math.pow(i, 2));
-            m=m.multiply(a);
-        }
-        return m;
+    public static BigDecimal f(int n) {
+        if (n==1)
+            return BigDecimal.ONE;
+        else return (new BigDecimal(n)).multiply((new BigDecimal(n)).multiply(f(n-1)));
     }
-
+    
     public static void main(String[] args) {
-        System.out.println("Multiplication of squares of numbers from 1 to 200 equals " + f());
+        System.out.println("Multiplication of squares of numbers from 1 to 200 equals " + f(200));
     }
 }
