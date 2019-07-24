275c822ae60d76d99e0537cf5fe08f4af153264f
Ivan Shilyaev <shilyaev1433@gmail.com>
2019-07-24 23:49:21 +0300

    arrays done

---
+++ 2_Algorithmization/1_one_dimensional_arrays/1-7.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            int n;
+            do {
+                System.out.println("Enter positive even integer n - number of elements:");
+                n = sc.nextInt();
+            } while (n<=0 || n%2!=0);
+            int arr[] = new int[n];
+            for (int i=0; i<n; ++i)
+                arr[i] = -50 + (int)(Math.random() * 100); // [-50;50]
+            System.out.println("Original array:");
+            for (int a : arr)
+                System.out.print(a + " ");
+            System.out.println();
+            int max=arr[0]+arr[n-1];
+            for (int i=1; i<n/2; ++i) {
+                if (arr[i]+arr[n-i-1]>max)
+                    max=arr[i]+arr[n-i-1];
+            }
+            System.out.println("max(a_1+a_n, a_2+a_(n-1), ...) = " + max);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Incorrect input data");
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
